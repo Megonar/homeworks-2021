f70a6bf79d302ae2d41922f2923445fdf912107b
Megonar <krism-1998@mail.ru>
2021-04-25 16:25:42 +0300

    Add my methods Array map,select,each

---
+++ HW02/Ramanovich_Dzmitry/hw02.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module MyMethod
+  refine Array do
+    def my_map
+      my_arr = Array.new
+      for i in 0...size
+        my_arr << yield(self[i])
+      end
+      my_arr
+    end
+
+    def my_each
+        for i in 0...size
+        yield(self[i])
+      end
+      self
+    end
+
+    def my_select
+      arr = Array.new
+      for i in 0...size
+        arr << self[i] if yield(self[i])
+      end
+      arr
+    end
+  end
+end
