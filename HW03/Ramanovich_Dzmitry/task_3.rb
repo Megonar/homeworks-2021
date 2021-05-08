0c9a8478d27e56d951f29f950bffc7e4512bf3d1
Megonar <krism-1998@mail.ru>
2021-05-08 16:39:31 +0300

    add new file test.rb

--- HW03/Ramanovich_Dzmitry/task_3.rb
+++ HW03/Ramanovich_Dzmitry/task_3.rb
@@ -15,16 +15,3 @@ def method(str)
   str.each_cons(2) { |str_one, str_two| arr << Time.parse(str_two) - Time.parse(str_one) }
   arr.join(',')
 end
-
-puts task_3("2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
-    2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
-    2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
-    2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
-    2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
-    2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
-    2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
-    2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - Calling core with action: event
-    2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
-    2018-04-23 17:19:22.4 ubuntu-xenial[14319] Debug - Calling core with action: event
-    2018-04-23 17:19:25.9 ubuntu-xenial[14319] Debug - inside docker_handle_event
-    2018-04-23 17:19:48.6 ubuntu-xenial[14319] Debug - Calling core with action: event")
--- HW03/Ramanovich_Dzmitry/task_4.rb
+++ HW03/Ramanovich_Dzmitry/task_4.rb
@@ -6,6 +6,7 @@ def quantity(str)
 
   hash[:letters] = str.count('a-z')
   hash[:digits] = str.count('0-9')
-
-  puts hash
+  p hash
 end
+
+p quantity('123fds!')
