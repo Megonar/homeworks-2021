4b0cc1243859fde34d9f46a10a9ed49557ba4f12
Megonar <krism-1998@mail.ru>
2021-05-11 21:09:22 +0300

    delete comment in tast_2.rb and fix bugs task_1.rb(new method str.first)

--- HW03/Ramanovich_Dzmitry/task_1.rb
+++ HW03/Ramanovich_Dzmitry/task_1.rb
@@ -3,4 +3,5 @@
 def task_1(data)
   str = data.split("\n")
   str.map { |line| line.downcase.include?('error') ? line : '' }.join('')
+  str.first
 end
--- HW03/Ramanovich_Dzmitry/task_2.rb
+++ HW03/Ramanovich_Dzmitry/task_2.rb
@@ -18,6 +18,3 @@ def task2(example)
   end
   array
 end
-
-
-#/(?<=\[)[^]]+(?=\])/
