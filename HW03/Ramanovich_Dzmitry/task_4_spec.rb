5f226bbde2f60330db3863e8da99afa2f04a6815
Megonar <krism-1998@mail.ru>
2021-05-21 23:29:13 +0300

    finished testing task_4.rb

--- HW03/Ramanovich_Dzmitry/task_4_spec.rb
+++ HW03/Ramanovich_Dzmitry/task_4_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe 'Test task_4' do
       expect(quantity('qwerty')).to eq({ digits: 0, letters: 6 })
     end
   end
+  context 'when input only symbol' do
+    it 'output 0 letters and 0 digits' do
+      expect(quantity('/////,.,.,')).to eq({ digits: 0, letters: 0 })
+    end
+  end
+  context 'when no arguments in input' do
+    it 'return Error' do
+      expect { quantity }.to raise_error(ArgumentError)
+    end
+  end
+  context 'when in input symbol, letters and digits' do
+    it 'return quantity only letters and digits' do
+      expect(quantity('123zxc///')).to eq({ digits: 3, letters: 3 })
+    end
+  end
 end
