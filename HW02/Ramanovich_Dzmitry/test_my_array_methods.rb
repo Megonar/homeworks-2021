6d82e31197580b2c653598033628f57091284fb4
Megonar <krism-1998@mail.ru>
2021-05-22 21:26:05 +0300

    delete comment and testing SSH KEY

--- HW02/Ramanovich_Dzmitry/test_my_array_methods.rb
+++ HW02/Ramanovich_Dzmitry/test_my_array_methods.rb
@@ -6,5 +6,5 @@ using MyArrayMethod
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 puts a.my_each { |x| p x * 2 }.inspect
-# puts a.my_select { |x| puts x * 2 }.inspect
-# puts a.my_map { |x| puts x * 2 }.inspect
+puts a.my_select { |x| puts x * 2 }.inspect
+puts a.my_map { |x| puts x * 2 }.inspect
