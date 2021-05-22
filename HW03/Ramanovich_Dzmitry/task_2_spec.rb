8d9e5aaeeba6a0dbb905a05e58e81f86e7d32b7a
Megonar <krism-1998@mail.ru>
2021-05-22 16:04:20 +0300

    fux bugs

--- HW03/Ramanovich_Dzmitry/task_2_spec.rb
+++ HW03/Ramanovich_Dzmitry/task_2_spec.rb
@@ -17,6 +17,14 @@ right_logs = [
 ]
 
 RSpec.describe 'Test task_2.rb' do
+  let(:logs) do
+    <<~LOGS
+  10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+  10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
+  2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+  10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
+LOGS
+  end
   context 'when logs do not get the line' do
     it 'return empty array' do
       expect(task2('')).to eq([])
