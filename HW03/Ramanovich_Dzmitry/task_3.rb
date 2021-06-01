a9918cbeb320a9f02d6bbec1ef1a852e9b545cbd
Megonar <krism-1998@mail.ru>
2021-06-01 20:09:47 +0300

    moved if method to private method

--- HW03/Ramanovich_Dzmitry/task_3.rb
+++ HW03/Ramanovich_Dzmitry/task_3.rb
@@ -4,20 +4,15 @@ require 'time'
 TIME = /^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d/.freeze
 
 def task_3(logs)
-  array = []
   lines = logs.split(/\n/)
   str = lines.select { |line| line.include?('Calling') }
   str.map { |line| line[TIME] }
-  array << if str.empty?
-             zero(str)
-           else
-             subtraction(str)
-           end
+  check(str)
 end
 
 private
 
-def zero(_str)
+def zero(str)
   '0'
 end
 
@@ -25,4 +20,13 @@ def subtraction(str)
   str.each_cons(2).map do |str_one, str_two|
     Time.parse(str_two) - Time.parse(str_one)
   end.join(',')
+end
+
+def check(str)
+  array = []
+  array << if str.empty?
+             zero(str)
+           else
+             subtraction(str)
+           end
 end
